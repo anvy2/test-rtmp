b0d07d7d4244281eed18fc963b8e44173b1fd01a
Anshul <anshul2679@gmail.com>
2020-06-24 15:43:41 +0530

    test rtmp-server

---
+++ index.js
@@ -0,0 +1,18 @@
+const NodeMediaServer = require('node-media-server');
+const PORT = process.env.PORT || 5000;
+const config = {
+	rtmp: {
+		port: 1935,
+		chunk_size: 60000,
+		gop_cache: true,
+		ping: 30,
+		ping_timeout: 60
+	},
+	http: {
+		port: PORT,
+		allow_origin: '*'
+	}
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
